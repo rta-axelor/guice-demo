a70f77dab155b3b824f2bb91ba769f4e15fc7cd4
Rajvi <rta-axelor@gmail.com>
2022-04-06 16:56:49 +0530

    Added simple guice demo

--- src/main/java/com/axelor/guice/demo/CalculatorClient.java
+++ src/main/java/com/axelor/guice/demo/CalculatorClient.java
@@ -1,20 +1,18 @@
 package com.axelor.guice.demo;
 
 import com.axelor.guice.demo.module.CalculatorModule;
-import com.axelor.guice.demo.service.CalculatorService;
+import com.axelor.guice.demo.service.*;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
 public class CalculatorClient {
 	public static void main(String[] args) {
 		Injector injector = Guice.createInjector(new CalculatorModule());
-		CalculatorService calculatorService = injector.getInstance(CalculatorService.class);
-		System.out.println(calculatorService);
-		System.out.println("Addition: "+ calculatorService.calculate(10, 200,"add"));
-		System.out.println("Substraction: "+ calculatorService.calculate(4000, 200,"sub"));
-		System.out.println("Multiply: "+ calculatorService.calculate(1, 10,"mul"));
-		System.out.println("Division: "+ calculatorService.calculate(4000, 200,"div"));
-		CalculatorService calculatorService2 = injector.getInstance(CalculatorService.class);
-		System.out.println(calculatorService2);
+		Calculator calculator = injector.getInstance(Calculator.class);
+		System.out.println("Addition: "+ calculator.addition(10, 200));
+		System.out.println("Substraction: "+ calculator.substraction(4000, 200));
+		System.out.println("Multiply: "+ calculator.multiply(1, 10));
+		System.out.println("Division: "+ calculator.division(4000, 200));
+		
 	}
 }
